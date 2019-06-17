3b7be1320d40f2e4e7e8563be5b6a375334ac50f
Daniele Rizzo <calupietru@gmail.com>
2019-06-17 21:49:12 +0200

    Add files via upload

---
+++ mygprs.py
@@ -0,0 +1,16 @@
+import gsm,json
+from network import mqtt
+
+gsm.debug(True)
+gsm.start(tx=17,rx=16,apn="TM",roaming=True,wait=True)
+gsm.connect()
+gsm.status()
+gsm.ifconfig()
+62.22.27', '255.255.255.255', '192.168.254.254')
+client = mqtt("test1","calupietru.duckdns.org",port=1883,user="test1",password="test1")
+client.start()
+message = {"espid":"Prato","timestamp":None,"temperatura":23,"peso":70.1}
+client.publish("/maia/4",json.dumps(message))
+client.publish("/maia/4",json.dumps(message))
+client.stop()
+gsm.disconnect()
