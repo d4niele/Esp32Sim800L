55e4eda0da624291910a4269e2463115e3a6ae29
Daniele Rizzo <calupietru@gmail.com>
2019-07-07 11:28:46 +0200

    Update essedi.py

--- essedi.py
+++ essedi.py
@@ -1,3 +1,9 @@
 import uos
-uos.sdconfig(uos.SDMODE_SPI, clk=18, mosi=23, miso=19, cs=5)
+uos.sdconfig(uos.SDMODE_SPI, clk=18, mosi=23, miso=19, cs=5,maxspeed=16)
 uos.mountsd()
+
+import machine, ssd1306
+i2c = machine.I2C(scl=machine.Pin(22), sda=machine.Pin(21))
+oled = ssd1306.SSD1306_I2C(128, 32, i2c)
+oled.text('funziona!!! on', 0, 0)
+oled.show()
